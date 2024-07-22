17bb25684074e3099705e1301e063f464af14161
pcbandua <pcbandua@gmail.com>
2024-07-22 15:33:04 -0700

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+puts 4 * 1
+puts 29 + 1
+puts 15 - 3
+puts 1966 / 2
+
+
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+puts 4 x 1     #syntax error, unexpected tIDENTIFIER, expecting end-of-input (can't use x, got to use * for multiplication)
+29 + 1 puts
+puts 15 - 3
+Puts 1966 / 2
