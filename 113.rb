86bf9f293f82d3040cfe6a3159eeb0323a237e40
pcbandua <pcbandua@gmail.com>
2024-07-22 17:37:09 -0700

    Create 113.rb

---
+++ 113.rb
@@ -0,0 +1,23 @@
+# QUESTION 1
+# Write code to store 3 different numbers in three different variables.
+
+wiredmagazine = 2
+atlanticmagazine = 4
+fourfourtwomagazine = 6
+
+# Then write the code to print the three variables added together.
+
+puts wiredmagazine + atlanticmagazine + fourfourtwomagazine
+
+# QUESTION 2
+# If you run the code below, it will crash due to invalid variable names. 
+# Find the invalid variable names and delete the special characters 
+# (be sure to leave the special characters in the valid variable names alone).
+bob7 = 50
+bob = 50
+bob = 50
+@bob = 50
+bob_ = 50
+_bob = 50
+bob = 50
+$bob = 50
