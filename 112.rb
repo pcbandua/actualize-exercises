3bc407107b1cd4110c793b0be6d2e4e089fef54c
pcbandua <pcbandua@gmail.com>
2024-07-22 17:23:52 -0700

    Create 112.rb

---
+++ 112.rb
@@ -0,0 +1,42 @@
+# QUESTION 1
+# Write code to print hellohellohello using string addition.
+# Then write code to print hellohellohello again, this time using string multiplication.
+
+puts "hello"
+puts "hello" + "hello" + "hello"
+puts "hellohellohello" * 1
+
+#QUESTION 2
+# Predictions
+                #puts 9 + 3
+                #puts "9" + 3    #crash pediction
+                #puts 9 + "3"    #crash prediction
+                #puts "9" + "3"
+                #puts 9 - 3
+                #puts "9" - 3    #crash pediction    
+                #puts 9 - "3"    #crash pediction
+                #puts 9 * 3
+                #puts "9" * 3
+                #puts 9 * "3"    #crash pediction
+                #puts "9" * "3"  #crash pediction
+                #puts 9 / 3
+                #puts "9" / 3    #crash pediction
+                #puts 9 / "3"    #crash pediction
+                #puts "9" / "3"  #crash pediction
+
+
+#Results
+puts 9 + 3
+
+
+puts "9" + "3"
+puts 9 - 3
+
+
+puts 9 * 3
+puts "9" * 3
+
+
+puts 9 / 3
+
+
