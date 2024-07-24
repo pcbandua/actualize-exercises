93dff81b031bf8508750c059fe5e2485863f53cc
pcbandua <pcbandua@gmail.com>
2024-07-23 21:20:20 -0700

    Create 121.rb

---
+++ 121.rb
@@ -0,0 +1,11 @@
+# QUESTION 1
+m = "manchester United"                     
+puts m.capitalize                            #Returns string with capitalized first letter only
+
+puts "football".delete_suffix("ball")        #It prints a copy of the string without the specifcied suffix
+
+puts ".9".succ                                #It prints the next successive letter or number of the right furthermost character
+
+
+# QUESTION 2
+puts (-56  11 1).abs                            #Ruby didn't understand the integer due to spacing within it or "syntax error"
