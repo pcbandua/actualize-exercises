2e08f333f2f45642b03c40be56f0a06bdf7f3ef3
pcbandua <pcbandua@gmail.com>
2024-07-26 15:37:30 -0700

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+
+def haiku_for_weekend
+    puts "one short hour left"
+    puts "until i can blow out of"
+    puts "this popsicle stand"
+end
+
+puts "here's a little something to remember on friday @ 5"
+
+haiku_for_weekend  
+haiku_for_weekend 
+haiku_for_weekend 
+
