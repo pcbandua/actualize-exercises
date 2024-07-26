b4132109e6f404bb61eb0e523e05aeba9ab2b5d1
pcbandua <pcbandua@gmail.com>
2024-07-26 15:38:35 -0700

    Update 151.rb

--- 151.rb
+++ 151.rb
@@ -15,3 +15,17 @@ haiku_for_weekend
 haiku_for_weekend 
 haiku_for_weekend 
 
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+  end
+  
+  print_lyrics
+  
