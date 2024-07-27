1b3e60d6f2f8b68fa8e89c7cd328a225612f6a2c
pcbandua <pcbandua@gmail.com>
2024-07-27 10:55:18 -0700

    Create 152.rb

---
+++ 152.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Create a method that accepts three numbers as inputs, 
+# and returns the product of all three numbers. 
+# So, if the three inputs were 2, 4, and 6, the output should be 48.
+
+def cube_area (cube_height, cube_length, cube_width)
+
+    return cube_height * cube_length * cube_width
+end    
+
+# QUESTION 2
+# The following method is supposed to take in 3 numbers and return the sum.
+# However, there are mistakes in the code that cause it to crash.
+# Explain what the error message means in your own words, then fix the mistakes in the code.
+def add_three_numbers(number1, number2, number3)
+    return number1 + number2 + number3
+  end
+
+  numbers = [3, 2, 7]
+puts add_three_numbers(3, 2, 7)          #errors is that the numberic values weren't added in tio the add_three_numbers part
+
