5996d5b1189aaaa0bacbc910ccebd208288c8376
pcbandua <pcbandua@gmail.com>
2024-07-30 21:08:01 -0700

    Create 211.rb

---
+++ 211.rb
@@ -0,0 +1,25 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+ball = 89
+
+puts ball + 10
+
+# Were you able to easily solve the problem from memory?
+    # yes
+# If so, describe the next skill you will start to practice tomorrow.
+    #variables
+# If not, describe the part you got stuck on that requires more practice.
+
+
+#Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+x = "ball"
+
+puts x.upcase
+
+# Were you able to easily solve the problem from memory?
+    # no
+# If so, describe the next skill you will start to practice tomorrow.
+   
+# If not, describe the part you got stuck on that requires more practice.
+    # I forgot that ".upcase" needed to be used. I used ".capital" by mistake. 
