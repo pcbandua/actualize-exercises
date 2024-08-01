d5ce99006c67acca9db88bd52ed8d41d02f5f528
pcbandua <pcbandua@gmail.com>
2024-07-31 20:38:21 -0700

    Add exercise solutions

---
+++ 221.rb
@@ -0,0 +1,30 @@
+#Write a program that uses a variable to store a number, then prints the number times 10.
+
+honk = 5332
+
+puts honk * 10
+
+
+# Were you able to easily solve the problem from memory?
+#yes
+# If so, describe the next skill you will start to practice tomorrow.
+#variables
+# If not, describe the part you got stuck on that requires more practice.
+
+
+
+
+
+#Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+t = "strawberry"
+w = "ice_cream"
+puts t.upcase + w.upcase
+
+
+
+# Were you able to easily solve the problem from memory?
+#yes
+# If so, describe the next skill you will start to practice tomorrow.
+#variables
+# If not, describe the part you got stuck on that requires more practice.
