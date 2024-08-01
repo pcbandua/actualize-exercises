23549643aa9e9ead07d2e96b3ba68a2934575889
pcbandua <pcbandua@gmail.com>
2024-08-01 10:59:08 -0700

    Add exercise solutions

---
+++ 231.rb
@@ -0,0 +1,23 @@
+#Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+x = 67 
+j = 42
+
+puts x * j
+
+
+# Were you able to easily solve the problem from memory? No
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. 
+# I interpreted the question to mean, stroging two numbers within one variable similar to an array. I want tokeep practicing variables. 
+
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+g = "EaSe"
+puts g.downcase 
+
+# Were you able to easily solve the problem from memory? No
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. 
+# I used .lowercase instead of .downcase. 
