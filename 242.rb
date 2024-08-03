e1076c0274ab77a55eb5b88af1e4e840ed52b561
pcbandua <pcbandua@gmail.com>
2024-08-03 01:58:59 -0700

    Add exercise solutions

---
+++ 242.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+travel = ["Bahamas", "Angola", "Macau"]
+travel.push("Texas")
+
+pp travel
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+
+food_specialties = {"Texas" => "BBQ", "Senegal" => "Jollof"}
+
+food_specialties ["Japan"] = "Sushi"
+
+pp food_specialties
