98f393ddb8a55715e30853ffcd425037f21411a9
pcbandua <pcbandua@gmail.com>
2024-08-03 01:46:56 -0700

    Add exercise solutions

---
+++ 241.rb
@@ -0,0 +1,10 @@
+#Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+r = "retention_bonus"
+
+puts r.reverse
+
+
+# Were you able to easily solve the problem from memory? Yes
+# If so, describe the next skill you will start to practice tomorrow. If Statements
+# If not, describe the part you got stuck on that requires more practice.
