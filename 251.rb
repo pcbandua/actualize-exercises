60214b47b2c6d78dddae3f1d08f6a83c9fce6907
pcbandua <pcbandua@gmail.com>
2024-08-03 22:53:57 -0700

    Add exercise solutions

---
+++ 251.rb
@@ -0,0 +1,15 @@
+#Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+#person = {first_name: "Maxine", last_name: "Mags", email: "doublem@gmail.com"}
+
+person = {"first_name" => "Maxine", "last_name" => "Mags", "email" => "doublem@gmail.com"}
+
+puts person ["first_name"]
+puts person ["last_name"]
+puts person ["email"]
+
+
+# Were you able to easily solve the problem from memory? Almost, 80% there
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. I tried ot wirte it out as an intermediate hash and got stuck. 
+
