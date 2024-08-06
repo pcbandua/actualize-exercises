6938ae1fe4292bf93a12e781db745a93868d2e34
pcbandua <pcbandua@gmail.com>
2024-08-06 16:50:16 -0700

    Add exercise solutions

---
+++ 311.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+#Write a while loop to print the numbers 1 through 10.
+
+index = 1
+while index <= 10
+    puts index
+index = index + 1
+end
+
+
+
+# Were you able to easily solve the problem from memory? no
+# If so, describe the next skill you will start to practice tomorrow. 
+# If not, describe the part you got stuck on that requires more practice. I needto keep practicing loops to get the syntax down.
+
+
