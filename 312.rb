e613583c6122a74410476e33088eec818fd19a55
pcbandua <pcbandua@gmail.com>
2024-08-07 11:45:57 -0700

    Add exercise solutions

---
+++ 312.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Use a loop to print out only the numbers greater than 10.
+numbers = [4, 53, 22, 3, 9, 7, 12]
+
+
+#Create a while loop referncing the numbers in the array greater than 10 
+while numbers.each >= 10
+ 
+end
+
+#Add a program to print of numbers
+
