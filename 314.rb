9370462e16f88f0ca805993cf16b8221321301a1
pcbandua <pcbandua@gmail.com>
2024-08-08 19:08:07 -0700

    Add exercise solutions

---
+++ 314.rb
@@ -0,0 +1,34 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Ancay ouyay igurefay histay neoay utoay?
+# Pig Latin is a very sophisticated language in which 
+# each English word is converted as follows: 
+# The first letter of each word is removed from the beginning and added to the end. 
+# Then, the letters 'ay' are added to the very end of the word.
+# For example: hello = ellohay, pingpong = ingpongpay, marmalade = armalademay, etc.
+# Write a program that asks the user to enter a single word 
+# and prints out the Pig Latin version of that word.
+
+
+#write program to ask user to enter question
+# puts "Enter a word to turn it into Pig Latin"
+
+#Give user opportunity to answer question
+puts "Enter a word to turn it into Pig Latin"
+puts input_word = gets.chomp
+
+#write program that turns the string into an array
+pig_latin = input_word.split(//)
+puts pig_latin
+
+#write program that takes the answer and removes the first letter of the word
+
+
+#write program that takes the first letter ofthe word and adds it to the end of the word
+
+#write program that adds the letters 'ay' to the end of the word
+
+#write program that prints out the Pig Latin word
