f8a256c7349e80e5465d0c54f62d463acceb0fd6
pcbandua <pcbandua@gmail.com>
2024-08-09 11:53:16 -0700

    Add exercise solutions

---
+++ 332.rb
@@ -0,0 +1,14 @@
+
+# QUESTION 1
+# Use a loop to compute the sum of all the given numbers.
+numbers = [5, 4, 2, 2, 6, 8, 1]
+
+index = 0
+sum_numbers = 0
+while index < numbers.length
+    sum_numbers = sum_numbers + numbers[index]  
+    index = index + 1
+ 
+end
+p sum_numbers
+
