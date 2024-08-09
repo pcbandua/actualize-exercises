86a866e901a2cefd7bf7343a3fa6055dcdc19258
pcbandua <pcbandua@gmail.com>
2024-08-09 11:59:51 -0700

    Add exercise solutions

---
+++ 333.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Create a program that asks the user to enter 5 numbers. 
+# Then, tell the user the mean average of all the numbers.
+# The mean average is defined as the sum of all the numbers 
+# divided by the count of how many numbers there are. 
+# For example, if the user enters: 11, 7, 30, 22, 55
+# the average will be 25. This is because 11 + 7 + 30 + 22 + 55 = 125, and 125 / 5 
+# (the amount of numbers in the list) = 25.
+
+# Note: If you run the code p 9 / 2, it prints 4 even though the result should be 4.5. 
+# This is because when dividing integers in Ruby, the result will be an integer. 
+
+puts "Enter what you regard as your 5 most lucky numbers"
+lucky_numbers = gets.chomp
+
+avg_numbers = lucky_numbers.sum
+puts "the mean average of your luckiest numbers is #{avg_numbers}"
