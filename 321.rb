fb784df354cbb897879979a6b73f8836e31d046d
pcbandua <pcbandua@gmail.com>
2024-08-08 19:20:27 -0700

    Add exercise solutions

---
+++ 321.rb
@@ -0,0 +1,53 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+# h = 6
+
+# if h == 10
+#     puts 0
+# end
+# elsif h != 10
+#     puts -1
+# end
+
+h = 6
+
+if h == 10
+    puts 0
+
+else
+    puts -1
+end
+
+
+# Were you able to easily solve the problem from memory? No, I was about 50% there. 
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. Understanding the syntax of if statements
+
+
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Start with an array of numbers and create a new array with each number times 3.
+
+# floors = [4, 5, 6, 7]
+
+# new_floors = floors.each * 3
+
+floors = [4, 5, 6, 7]
+new_floors = []
+floors.each do |number|
+    new_floors << number * 3
+end
+p new_floors
+
+# Were you able to easily solve the problem from memory? No, I was about 50% there. 
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. Understanding the syntax of if statements
+
+
+
