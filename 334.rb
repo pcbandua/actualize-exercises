b8bb43f3e7b215132f2d0264010016304227a4d0
pcbandua <pcbandua@gmail.com>
2024-08-10 00:14:22 -0700

    Add exercise solutions

---
+++ 334.rb
@@ -0,0 +1,28 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Have the user enter 10 words, and allow for duplicate words. 
+# After the user is done, the program will tell the user 
+# which word was entered the most frequently.
+# For example, if the user enters: 
+#   apple, banana, orange, pear, apple, pear, apple, squash, apple, pear
+# The program will say: "apple was your most common word"
+# (That's because apple appeared in the user's list 4 times, more than any other word.)
+
+
+
+#write  program that sends a message to the user asking them to enter 10 words with duplicates allowed
+puts "Write down 10 words that descriobe the most common emotions you feel most frequently as you learn software engineering (duplicates allowed)"
+
+#write a program that lets the user enter the 10 words
+puts "Write down 10 words that descriobe the most common emotions you feel most frequently as you learn software engineering (duplicates allowed)"
+swe_emotions = gets.chomp
+
+#write a program that checks for any duplucated words 
+
+
+#write a program that recognizes the most duplciated word
+
+#write a program that prints the most duplicated word for the user to view
