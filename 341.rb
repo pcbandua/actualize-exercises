54eabc2317f172658eacb679a808c57a5282c2e2
pcbandua <pcbandua@gmail.com>
2024-08-10 10:31:09 -0700

    Add exercise solutions

---
+++ 341.rb
@@ -0,0 +1,25 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+#Start with an array of numbers and compute the sum of all the numbers.
+
+numbers = [10, 509, 3]
+
+sum = 0 
+index = 0
+
+while index < numbers.length
+    sum = sum + numbers[index]
+    index = index + 1
+end
+
+puts sum
+
+# Were you able to easily solve the problem from memory? no
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. loops
+
+
+
