672e1752c9d56cf28d286a8add55ddbf8e9e5119
pcbandua <pcbandua@gmail.com>
2024-08-11 20:24:11 -0700

    Add exercise solutions

---
+++ 351.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+#Start with an array of strings and combine them all into a single string.
+fav_striker = ["Ruud Van Nistelroy, ", "Manchester United, ", "Striker, "] 
+
+puts fav_striker.join
+
+# Were you able to easily solve the problem from memory? Yes
+# If so, describe the next skill you will start to practice tomorrow. Conditional loops
+# If not, describe the part you got stuck on that requires more practice.
+
+
