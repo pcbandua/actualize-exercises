3d4c2fce760e711456aad76344e03c1c7cfe634f
pcbandua <pcbandua@gmail.com>
2024-08-11 21:06:10 -0700

    Add exercise solutions

---
+++ 352.rb
@@ -0,0 +1,18 @@
+# QUESTION 1
+# Use a loop to create a new array with each string's first letter only.
+
+
+words = ["Big", "Elephants", "Can", "Always", "Understand", "Small", "Elephants"]
+
+index = 0
+first_letters = []
+
+while index < words.length
+
+    word = words[index]
+    first_letters = word  #Feel that the issue lies here, because the code prints the string but not the first letter of the first string in the array. 
+    index = index + 1
+end
+
+pp first_letters
+
