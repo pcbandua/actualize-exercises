71b9c464a73055dd4a6a9116201ca48de5317511
Paulo Bandua <pcbandua@gmail.com>
2024-08-13 20:49:57 -0700

    Add exercise solutions

---
+++ 411.rb
@@ -0,0 +1,25 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Write a method that takes in a number and returns the number times two. Then run the method and print the result.
+
+# def initialize
+#     puts "enter number"
+#     number = gets.chomp
+#     new_number = number * 2
+# end
+# puts new_number
+
+def new_number(number)
+    return number * 2
+  end
+  
+  pp new_number(8)
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
