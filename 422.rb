d18a9242fd4db88007b68509c80abe178cac93d3
Paulo Bandua <pcbandua@gmail.com>
2024-08-14 16:41:38 -0700

    Add exercise solutions

---
+++ 422.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Write a method that takes in a string and a number n and returns the string repeated n times.
+# Then run the method and print the result.
+
+phrase = "She sells seashells by the seashore."
+
+def tongue_twister(phrase)
+
+    number = 12
+
+    number.times do
+        puts phrase
+    end
+   
+end
+
+puts tongue_twister(phrase)
