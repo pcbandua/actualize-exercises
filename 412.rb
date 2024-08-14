4b9c7dcfd4ddd5cb0c2cf6fae8a3d313042b03d4
Paulo Bandua <pcbandua@gmail.com>
2024-08-14 16:03:22 -0700

    Add exercise solutions

---
+++ 412.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns its sum.
+# Then run the method and print the result.
+
+
+clients_monthly = [2, 1, 9, 18, 15, 6, 3, 9, 1, 20, 30, 13]
+
+def annual_total(clients_monthly)
+    clients_monthly.sum
+end
+
+puts annual_total(clients_monthly)
