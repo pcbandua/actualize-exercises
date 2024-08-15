4e3f0fcca3bc3a15b1d0ea685a85c6ea8fecc439
Paulo Bandua <pcbandua@gmail.com>
2024-08-14 17:29:06 -0700

    Add exercise solutions

---
+++ 431.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Start with an array of numbers and create a new array with each number times 3.
+# For example, [1, 2, 3] becomes [3, 6, 9].
+
+breathwork_timing = [4, 2, 8, 4, 2, 8, 4, 2, 8]
+
+new_array = breathwork_timing.map { |breathwork_timing| breathwork_timing * 3}
+pp new_array
+
+# Were you able to easily solve the problem from memory? no, but I was really close.
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. intermediate looping
+
+
