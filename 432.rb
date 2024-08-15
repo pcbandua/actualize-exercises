58583b48aa39de95617c0ad97bf4b552d40adc3a
Paulo Bandua <pcbandua@gmail.com>
2024-08-14 17:45:46 -0700

    Add exercise solutions

---
+++ 432.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns the mean average.
+# Then run the method and print the result.
+
+heights = [5, 4, 6, 6, 5]
+
+# def phone_heights(mean)
+#     sum = heights.sum
+#    return mean = sum / heights.length
+# end
+
+# puts phone_heights(mean)
+
+def phone_heights
+    sum = heights.sum
+   return mean = sum / heights.length
+end
+
+puts phone_heights
