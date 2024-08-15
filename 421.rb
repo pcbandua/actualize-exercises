d42a34bb7e2514db360544e423978b91c4d7db83
Paulo Bandua <pcbandua@gmail.com>
2024-08-14 21:21:39 -0700

    Add exercise solutions

--- 421.rb
+++ 421.rb
@@ -2,9 +2,19 @@
 # Paste a deliberate practice question you are currently working on below.
 # Write the code to solve the problem without looking at any notes or other resources.
 
+#Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-# Were you able to easily solve the problem from memory?
+home = 55
+
+if home == 10
+    puts 0
+
+else
+    puts -1
+end
+
+# Were you able to easily solve the problem from memory? Yes
 # If so, describe the next skill you will start to practice tomorrow.
-# If not, describe the part you got stuck on that requires more practice.
+# If not, describe the part you got stuck on that requires more practice. Loops
 
 
