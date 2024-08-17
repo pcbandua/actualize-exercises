4779c7ed8931f2f595ec1bfbca38addb0fa97dbe
Paulo Bandua <pcbandua@gmail.com>
2024-08-16 18:25:41 -0700

    Add exercise solutions

---
+++ 441.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+first = "Didier "
+last = "Drogba"
+
+pp first + last
+
+# Were you able to easily solve the problem from memory? Yes
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.Conditional loops
+
+
