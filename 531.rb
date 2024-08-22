fad58005c147d1397f1b1845341de88233b4772f
Paulo Bandua <pcbandua@gmail.com>
2024-08-21 21:11:50 -0700

    Add exercise solutions

---
+++ 531.rb
@@ -0,0 +1,27 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a while loop to print the numbers 1 through 10.
+
+# number = []
+# index = 0
+
+# while index <= 10
+# puts number[index] + 1
+# index = index + 1
+# end
+
+number =1 
+
+while number <= 10
+    pp number
+    number = number + 1
+
+end
+
+# Were you able to easily solve the problem from memory? Almost but no
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.loops
+
+
