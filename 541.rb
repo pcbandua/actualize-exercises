36690c28a109b6e00cac0a53a774df90690883a7
Paulo Bandua <pcbandua@gmail.com>
2024-08-24 09:40:23 -0700

    Add exercise solutions

---
+++ 541.rb
@@ -0,0 +1,25 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# while 1 < 2
+#     puts "word loop"
+    
+# end
+
+while true
+    puts "Enter your fav word"
+    input = gets.chomp
+    if input == "stop"
+        break
+    end
+end
+
+
+# Were you able to easily solve the problem from memory? No
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. Looping
+
+
