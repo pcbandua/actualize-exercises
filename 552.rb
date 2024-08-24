f408887ae085c9df62e93af4514a09b0260f5e07
Paulo Bandua <pcbandua@gmail.com>
2024-08-24 11:08:42 -0700

    Add exercise solutions

---
+++ 552.rb
@@ -0,0 +1,13 @@
+# QUESTION 1
+# Write a method that takes in three strings and uses string interpolation 
+# to return all three strings combined with spaces in between as a single string. 
+# Then run the method and print the result.
+
+
+
+def join_strings
+    strings = ["Laugh " "Destiny " "Resilience "]
+    puts strings.join
+end
+
+join_strings
