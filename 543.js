2281db6520ec279b5ef454570ffc9290af142dab
Paulo Bandua <pcbandua@gmail.com>
2024-08-24 10:00:13 -0700

    Add exercise solutions

---
+++ 543.js
@@ -0,0 +1,27 @@
+// QUESTION 1
+// Write code to store an array of numbers.
+// The print out the last item in that array.
+
+var array = [7, 444, 555, 1040, 6334, 222]
+
+console.log(array [5]);
+
+
+// QUESTION 2
+// Write code to store a number in a variable.
+// Then write a condition that 
+// prints -1 if the number is less than 10, 
+// prints 1 if the number is greater than 10, 
+// and prints 0 if the number is equal to 10.
+
+var ppp = 444
+
+if (ppp < 10)
+{console.log (-1);}
+
+    else if (ppp > 10)
+    {console.log (1);}
+
+    else {console.log (0);}
+
+
