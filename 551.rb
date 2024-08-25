30e409ca5e823dfc053e38fb265abc534e9515e5
Paulo Bandua <pcbandua@gmail.com>
2024-08-24 22:47:32 -0700

    Add exercise solutions

---
+++ 551.rb
@@ -0,0 +1,23 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+x =  45003
+
+if x == 10
+    puts 0
+
+else
+    puts -1
+
+end
+
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
